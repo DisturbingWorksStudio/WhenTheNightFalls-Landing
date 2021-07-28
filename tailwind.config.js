bb16b420fd2d3a3812b7183c32f9e94c1c2cad47
kkaplinskii <krzysztof.kaplinski@tunego.com>
2021-07-28 19:57:22 +0200

    feat: gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,35 @@
 module.exports = {
   purge: [
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.html',
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.html",
   ],
   darkMode: false,
   theme: {
     container: {
       screens: {
-          'sm': '640px',
-          'md': '768px',
-          'lg': '1024px',
-      }
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+      },
     },
-    extend: {},
+    gradientColorStops: (theme) => ({
+      ...theme("colors"),
+      primary: "#1C0707",
+    }),
+    extend: {
+      animation: {
+        "zoom": "zoom 0.3s ease-in",
+      },
+      keyframes: {
+        zoom: {
+          "0%": { transform: "scale(0.4)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+    },
   },
   variants: {},
-  plugins: [
-  ],
-}
+  plugins: [],
+};
